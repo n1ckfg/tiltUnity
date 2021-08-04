82efd47159d98794357eed63053662da273277ba
Nick Fox-Gieg <nick@fox-gieg.com>
2021-08-04 17:25:32 -0400

    setup

---
+++ Assets/Scripts/TiltLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltLoader : MonoBehaviour {
+
+    public string url;
+    public byte[] bytes;
+    public JSONObject json;
+    public int numStrokes;
+    public List<TiltStroke> strokes;
+
+    private ZipFile zipFile;
+    private ArrayList<String> fileNames;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+}
---
+++ Assets/Scripts/TiltStroke.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltStroke : MonoBehaviour {
+
+    public List<Vector3> positions;
+    public float brushSize;
+    public Color brushColor;
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+}
